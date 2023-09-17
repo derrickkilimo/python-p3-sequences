af41de78c6ab04b37750fbf193c934d6d28a0999
derrickkilimo <derrick.kilimo@student.moringaschool.com>
2023-09-17 19:09:25 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a = 0
+    b = 1
+    list_fib = []
+
+    if length == 0:
+        print(list_fib)
+    else:
+        for _ in range(length):
+            c = a + b
+            list_fib.append(a)
+            a, b = b, c
+
+        print(list_fib)
+
+# Example usage:
+print_fibonacci(1)
